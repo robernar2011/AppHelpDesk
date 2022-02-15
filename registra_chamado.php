02a6047a28855a6deb2de99c055b7db73f10afb1
robernar2011 <robernar2011@gmail.com>
2022-02-15 15:49:14 -0300

    Open call and page to register call

---
+++ registra_chamado.php
@@ -0,0 +1,26 @@
+<?php
+
+	print_r($_POST);
+
+	//Montagem do texto a ser escrito
+
+	$titulo = str_replace('#', '-', $_POST['titulo']);
+	$categoria = str_replace('#', '-', $_POST['categoria']);
+	$descricao = str_replace('#', '-', $_POST['descricao']);
+
+	//EOL = End of line
+	$texto = $titulo . '#' . $categoria . '#' . $descricao . PHP_EOL;
+
+
+	//Função nativa do PHP para abrir arquivo
+	$arquivo = fopen('arquivo.hd', 'a');
+
+	//Função para escrever arquivo
+	fwrite($arquivo, $texto);
+
+	//Função para fechar arquivo
+	fclose($arquivo);
+
+	//Retornar para a página Abrir Chamado
+	header('Location: abrir_chamado.php');
+?>

e6f7a222b4c5fb1c852434440f4f7ce7fc1509d9
robernar2011 <robernar2011@gmail.com>
2022-02-15 11:29:15 -0300

    User autentication

--- index.php
+++ index.php
@@ -40,7 +40,16 @@
 								</div>
 								<div class="form-group">
 									<input name="senha" type="password" class="form-control" placeholder="Senha">
+								</div>	
+
+								<?php if(isset($_GET['login']) && $_GET['login'] == 'erro') { ?>
+
+								<div class="text-danger">
+									Usuário e/ou Senha Inválido(s)
 								</div>
+
+								<?php } ?>
+
 								<button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
 							</form>
 						</div>
--- valida_login.php
+++ valida_login.php
@@ -1,10 +1,24 @@
 <?php 
 
-	print_r($_POST);
+	//Verificar se autenticação foi realizada
+	$usuario_autenticado = false;
 
-	echo '<br />';
+	//Usuários do Sistema (Hardcode, ainda sem Banco de Dados)
+	$usuarios_app = array(
+		array('email' => 'adm@teste.com.br', 'senha' => '123456'),
+		array('email' => 'user@teste.com.br', 'senha' => 'abcd'),
+	);
 
-	echo $_POST['email'];
-	echo '<br />';
-	echo $_POST['senha'];
+	foreach ($usuarios_app as $user) {
+		if ($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']) {
+			$usuario_autenticado = true;
+		}
+	}
+
+	if ($usuario_autenticado) {
+		echo 'Usuário Autenticado';
+	} else{
+		header('Location:index.php?login=erro');
+	}
+
  ?>

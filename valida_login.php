50f72c0cdca8b977e6cbdcbd0f75489428676f75
robernar2011 <robernar2011@gmail.com>
2022-02-15 13:29:16 -0300

    Navigation between pages

--- valida_login.php
+++ valida_login.php
@@ -20,6 +20,7 @@
 	if ($usuario_autenticado) {
 		echo 'Usuário Autenticado';
 		$_SESSION['autenticado'] = 'SIM';
+		header('Location: home.php');
 	} else{
 		$_SESSION['autenticado'] = 'NAO';
 		header('Location: index.php?login=erro');
